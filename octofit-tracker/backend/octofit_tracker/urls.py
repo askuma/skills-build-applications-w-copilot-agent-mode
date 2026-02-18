8f7a824019f3d2843912219a6944b7af00eebfe6
askuma <48277068+askuma@users.noreply.github.com>
2026-02-18 13:11:15 +0000

    First commit based on changes done by Github pilot.

---
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -0,0 +1,30 @@
+"""octofit_tracker URL Configuration."""
+import os
+
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.routers import DefaultRouter
+
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
+
+router = DefaultRouter()
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'admin': f"{base_url}{reverse('admin:index')}",
+    })
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', api_root, name='api-root'),
+    path('api/', include(router.urls)),
+]
